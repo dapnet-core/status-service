5ca81ae28f6b767b94dc0fe0b35f29b8d7f680a5
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-10 22:17:39 +0200

    Added first json handling, not beautiful

--- status-service.py
+++ status-service.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import jsonify
+import json
+import platform
 
 app = Flask(__name__)
 api = Api(app)
 
 class Status(Resource):
     def get(self):
-        return {'hello': 'world'}
+        data = {}
+        data['hello'] = 'world'
+        data['python_version'] = platform.python_version()
+        return json.dumps(data)
 
 api.add_resource(Status, '/status')
 
