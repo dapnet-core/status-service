67676b643dba48f4f504fca785cfa06d45f91b12
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-10 22:53:53 +0200

    Changed Port in container from 5000 to 80

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ ADD status-service.py  /
 RUN pip3 install flask && pip3 install flask-restful
 
 # private and public mapping
-EXPOSE 5000
+EXPOSE 80
 
 CMD [ "python", "./status-service.py" ]
--- status-service.py
+++ status-service.py
@@ -15,5 +15,5 @@ class Status(Resource):
 api.add_resource(Status, '/status')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
 
