9b28f40a26bec879c341db8063018f6f80f29584
root <root@dapnetdc1.db0sda.ampr.org>
2018-07-11 12:22:20 +0200

    Changed to resources handling

--- status-service.py
+++ status-service.py
@@ -1,18 +1,20 @@
+
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+#from flask_restful import Resource, Api
 import platform
 
 app = Flask(__name__)
-api = Api(app)
 
-class Status(Resource):
-    def get(self):
-        data = {}
-        data['hello'] = 'world'
-        data['python_version'] = platform.python_version()
-        return (data)
+@app.route('/status')
+def api_status():
+    data = {}
+    data['hello'] = 'world'
+    data['python_version'] = platform.python_version()
+    return jsonify(data)
 
-api.add_resource(Status, '/status')
+@app.route('/status/<servicename>')
+def api_status_details(servicename):
+    return 'You are reading ' + servicename
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
